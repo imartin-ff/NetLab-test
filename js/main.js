596c6930bddd266b861b0ae4017d037a675267d3
MaRTiN <53973885+imartin-ff@users.noreply.github.com>
2021-08-04 17:47:56 +0300

    finally

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+    const swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: 'true'
+        },
+    })
+})
